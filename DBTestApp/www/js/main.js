35eabcf48f76d8857f069fe2e18e4b0b1181ff34
j6u6g6g6a6l6o <pj_icp@hotmail.com>
2016-02-27 11:04:05 -0400

    Added css for button look & click events, more database setup & functionality

--- DBTestApp/www/js/main.js
+++ DBTestApp/www/js/main.js
@@ -16,3 +16,45 @@ function setup(tx) {
 function errorHandler(e) {
     alert(e.message);
 }
+
+function dbReady() {
+
+    $("#btnAdd").on("touchstart", function(e) {
+        db.transaction(function(tx) {
+            var msg = "Log it...";
+            var d = new Date();
+            d.setDate(d.getDate() - randRange(1, 30));
+            tx.executeSql("insert into log(log, created) values(?, ?)", [msg, d.getTime()]);
+        }, errorHandler, function() {
+            $("output").html("Added a row");
+        });
+    });
+
+    $("#btnDelete").on("touchstart", function(e) {
+        db.transaction(function(tx) {
+            tx.executeSql("delete from log");
+        }, errorHandler, function() {});
+    });
+
+    $("#btnTest").on("touchstart", function(e) {
+        db.transaction(function(tx) {
+            tx.executeSql("select * from log order by created desc", [], gotLog, errorHandler);
+        }, errorHandler, function() {});
+    });
+}
+
+function gotLog(tx, results) {
+    if (results.rows.length == 0) {
+        $("#output").html("No Data...");
+        return false;
+    }
+
+    var s = "";
+    for (var 1=0; i<results.rows.length; i++) {
+        var d = new Date();
+        d.setTime(results.rows.item(i).created);
+        s += d.toDateString() + " " + d.toTimeString() + "<br/>";
+    }
+
+    $("#output").html(s);
+}
