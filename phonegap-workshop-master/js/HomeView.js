cc18b63c85b3ccc4fb896df00db04e21515bd2be
j6u6g6g6a6l6o <pj_icp@hotmail.com>
2016-03-07 13:04:49 -0400

    Implemented iScroll object

--- phonegap-workshop-master/js/HomeView.js
+++ phonegap-workshop-master/js/HomeView.js
@@ -3,18 +3,29 @@ var HomeView = function(store) {
         this.initialize = function() {
         // Div wrapper for the view - used to attach events
         this.el = $('<div/>');
+        // Event listener
         var that = this;this.el.on('keyup', '.search-key', function() {that.findByName();});
     };
-    this.initialize();
 
+    this.initialize();
+    // Render the HomeView
     this.render = function() {
         this.el.html(HomeView.template());
         return this;
     };
-
+    // Find and display names that contain the characters typed by the user
     this.findByName = function() {
          store.findByName($('.search-key').val(), function(employees) {
         $('.employee-list').html(HomeView.liTemplate(employees));
+             // If an iScroll object already exists, use it
+             if (self.iscroll) {
+                 console.log('Refresh iScroll');
+                 self.iscroll.refresh();
+                 // If no iScroll object exists, create one
+             } else {
+                 console.log('New iScroll');
+                 self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+             }
     });
         /* console.log('findByName');
         var self = this;
