50807c17669b284e1bbcb95072abf7a8fc8874e6
j6u6g6g6a6l6o <pj_icp@hotmail.com>
2016-03-10 08:34:49 -0400

    Finished off the tutorial

--- phonegap-workshop-master/js/EmployeeView.js
+++ phonegap-workshop-master/js/EmployeeView.js
@@ -2,7 +2,12 @@ var EmployeeView = function(employee) {
     // Initialization
     this.initialize = function(){
         this.el = $('<div/>');
-        this.el.on('click', '.add-location-btn', this.addLocation);
+        // Event listener for Add Location button
+        this.el.on('click', '.btnLocation', this.addLocation);
+        // Event Listener for Add Contact button
+        this.el.on('click', '.btnContact', this.addToContacts);
+        // Event Listener for change picture button
+        this.el.on('click', '.btnPicChange', this.changePic);
     };
 
     // Render the employee View
@@ -27,6 +32,58 @@ var EmployeeView = function(employee) {
         return false;
 
     };
+
+    // Add to contacts event handler
+    this.addToContacts = function(e) {
+        e.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+
+        // Cretate a new contact
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        // Array to hold phone numbers
+        var phoneNumbers = [];
+        // Add the phone numbers to the array
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+        contact.phoneNumbers = phoneNumbers;
+        // Save the contact
+        contact.save();
+        return false;
+    };
+
+    this.changePicture = function(e) {
+        e.preventDefault();
+    if (!navigator.camera) {
+        app.showAlert("Camera API not supported", "Error");
+        return;
+    }
+    var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+        sourceType: 1,
+        // 0=JPG 1=PNG
+        encodingType: 0
+        };
+
+    navigator.camera.getPicture(
+        function(imageData) {
+            $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+        },
+        function() {
+            app.showAlert('Error taking picture', 'Error');
+        },
+        options);
+
+    return false;
+    };
+
+    // Event handler for changePic
 
     this.initialize();
 };
