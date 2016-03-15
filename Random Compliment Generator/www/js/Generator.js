f9b720e8bafc23447d07630f29a1d4ed14da6c36
j6u6g6g6a6l6o <pj_icp@hotmail.com>
2016-03-15 19:39:02 -0300

    Finished main functionality, Preparing to implement local storage

--- Random Compliment Generator/www/js/Generator.js
+++
@@ -1,5 +0,0 @@
-function Generator() {
-    var adjs = ["Awesome", "Great", "Super", "Perfect", "Intelligent", "Amazing", "Likable", "Cool"];
-    
-    var nouns = ["Person", "Human", "Friend"];
-}
--- Random Compliment Generator/www/js/main.js
+++ Random Compliment Generator/www/js/main.js
@@ -1,4 +1,4 @@
-var app {
+var app = {
     // Initialization
     initialize: function() {
         this.bindEvents();
@@ -9,7 +9,38 @@ var app {
     document.getElementById("btnComp").addEventListener('click', this.complimentMe, false); 
     },
     
+    complimentMe: function() {
+        // variables to hold the adjectives and noun
+        var adj1;
+        var adj2;
+        var adj3;
+        var noun;
+        // Get the user's name
+        var target = document.getElementById("target").value;
+        // Array of adjectives
+        var adjs = ["Awesome ", "Great ", "Super ", "Perfect ", "Intelligent ", "Amazing ", "Likable ", "Cool "];
+        // Array of nouns
+        var nouns = ["Person!", "Human!", "Friend!"];
+        var i = 0;
+        // Check if txtbox is empty
+        if (target !== "") {
+            
+            while (adj1 === adj2 || adj1 === adj3 || adj2 === adj3 || adj2 === adj1 || adj3 === adj1 || adj3 === adj2) {
+            adj1 = adjs[Math.floor(Math.random()*adjs.length)];
+            adj2 = adjs[Math.floor(Math.random()*adjs.length)];
+            adj3 = adjs[Math.floor(Math.random()*adjs.length)];
+            noun = nouns[Math.floor(Math.random()*nouns.length)];
+               console.log(target + " " + adj1 + adj2 +adj3 + noun);
+                
+            } document.getElementById("output").innerHTML = target + " is a " + adj1 + adj2 + adj3 + noun;
+            
+        } else {
+            alert("Please enter a name...");
+        }
+    }
     
+    
+
 };
 
 app.initialize();
