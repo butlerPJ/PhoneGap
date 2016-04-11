8d1bb582e28388091619acf9ec3df979fa863628
j6u6g6g6a6l6o <pj_icp@hotmail.com>
2016-04-11 09:57:25 -0300

    More interface work & set up class for ants

---
+++ Random Compliment Generator/www/js/Ant.js
@@ -0,0 +1,18 @@
+function Ant() {
+    "use strict";
+    
+    // ---------- Property Variables
+    var income = 1;
+    
+    // ---------- Get/Sets
+    this.getIncome = function() {
+        return income;
+    };
+    
+    this.setIncome = function(value) {
+        income = value;
+    };
+    
+    // ----------- Public Methods
+    
+}
